8df0ac9cfd5e006b003a8b47ade44103268a9dfa
tristanlorriaux <57456860+tristanlorriaux@users.noreply.github.com>
2019-12-04 00:19:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 
 int main() {
-    simulation(5000);
-    tps_rep_moy();
-    max_file();
-    moyenne_file();
+    float temps_simul=5000;
+    simulation(temps_simul);
+    float a = tps_rep_moy();
+    float b = max_file();
+    float c = moyenne_file();
     // pour renommer le txt en .dat
     FILE *file;
     file=fopen(NOM_FIC3, "r+");
     rename ("data_file.txt", "data_file.dat");
     fclose(file);
     system("gnuplot -persist -e \"plot '/home/tristan/P_C/cmake-build-debug/data_file.dat' title 'Nombre de voitures par file' with impulses\"");
+    tableau_de_bord(temps_simul,a,b,c);
     return 0;
 }
