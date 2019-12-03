32ba2759e0eb692f6222045b653809fc4f255e8e
tristanlorriaux <57456860+tristanlorriaux@users.noreply.github.com>
2019-12-03 21:56:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-#include "fonctions_aleatoires.h"
-#include "fonctions_utilitaires.h"
-#include "fonctions_init.h"
+#include <stdio.h>
 #include "fonctions.h"
 
+
 int main() {
-    
+    simulation(300);
+    tps_rep_moy();
+    max_file();
+    moyenne_file();
     return 0;
 }
